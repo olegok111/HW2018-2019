ca223051d03e7644d64aa0c92174ab4f5a2bf04c
olegok111 <olegok2003@mail.ru>
2018-11-04 17:11:15 +0300

    Update config.py

--- bot_mayak/config.py
+++ bot_mayak/config.py
@@ -1,4 +1,4 @@
-TOKEN = '662581590:AAE8SlvXuXnlCFzo325X0jBDSWbffvY-eos'
+TOKEN = 'There is no token'
 REQUEST_KWARGS = {'proxy_url': 'socks5://ru.socksy.seriyps.ru:7777',
     'urllib3_proxy_kwargs': {
         'username': 'tg-nondxm',
