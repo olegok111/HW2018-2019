8e3aed06bec014fa6c1073c262d6b6b9de96e809
olegok111 <olegok2003@mail.ru>
2019-04-21 13:50:16 +0300

    [B&W Image] v1.0

---
+++ pillow_conversion.py
@@ -0,0 +1,22 @@
+# TODO: custom conversion to two symbols
+# TODO: GUI
+
+from PIL import Image
+import easygui
+
+
+def zeros_and_ones(filename:str, zero='0', one='1'):
+    im = Image.open(filename)
+    im = im.convert('1')
+    size = im._size
+    for y in range(size[1]):
+        for x in range(size[0]):
+            if im.getpixel((x, y)):
+                print(one, end='')
+            else:
+                print(zero, end='')
+        print()
+
+n = easygui.fileopenbox(msg='Open an image you wish to convert')
+setting = easygui.multenterbox(msg='Выберите, какое значение будет нулём, а какое - единицей', fields=['0:', '1:'])
+zeros_and_ones(n, setting[0], setting[1])
