3192892d6bdcc835bc92d1332f0d50a6b50b71c3
olegok111 <olegok2003@mail.ru>
2019-05-31 17:59:02 +0300

    Add files via upload

---
+++ LSkKN/LShKN_e0.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int _len, _check_amount;
+    cin >> _len >> _check_amount;
+    const int len = _len+1-1, check_amount=_check_amount+1-1;
+    int cucheck, max_gap=-1, cugap=0;
+    char data[len];
+    cin >> data;
+    for (int i=0; i<len; i++) {
+        if (data[i] == '1') {
+            cugap++;
+            if (max_gap < cugap) {
+                max_gap = cugap;
+            }
+        } else {
+            cugap = 0;
+        }
+    }
+    vector <bool> answers(check_amount);
+    for (int i=0; i<check_amount; i++) {
+        cin >> cucheck;
+        if (cucheck <= max_gap) {
+            answers[i] = 0;
+        } else {
+            answers[i] = 1;
+        }
+    }
+    for (bool ans : answers) {
+        if (ans) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
---
+++ LSkKN/LShKN_f0.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    int sek, com;
+    cin >> sek >> com;
+    string sectors="", command="";
+    char c1, c2;
+    cin >> sectors;
+    vector <pair <char, char>> commands;
+    for (int i=0; i<com; i++) {
+        cin >> command;
+        if (command == "Forward") {
+            c1 = 'f';
+            cin >> c2;
+            // TODO: commands and arguments to vector
+            // TODO: locate the last element before GOTO command; it appears again => inf
+        }
+    }
+    return 0;
+}
